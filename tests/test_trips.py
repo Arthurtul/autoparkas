e4552ae07f9eeb22414b892b0d1166aa5e623316
Arturas Kryzanovskij <arturas.kryzanovskij@gmail.com>
2023-02-23 16:21:28 +0200

    Test of trips count method

---
+++ tests/test_trips.py
@@ -0,0 +1,26 @@
+import unittest
+from classes import Truck
+
+
+class MyTestCase(unittest.TestCase):
+    def setUp(self):
+        self.test_vehicle = Truck(
+            odometer=1000,
+            transported_weight=20,
+            trailer_carry=1,
+            vehicle_number="abc123",
+            gas_type="diesel",
+            costs=200,
+            drivers_license="AZ2424",
+            l_km=100,
+            tech_insp="2022-01-01",
+            insurance_date="2023-03-03",
+            gas_price=1.5
+        )
+
+    def test_calculate_trips(self):
+        self.assertEqual((2, 2), self.test_vehicle.calculate_trips(71))
+
+
+if __name__ == '__main__':
+    unittest.main()
